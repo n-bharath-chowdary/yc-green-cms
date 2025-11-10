6c999f587a6d547c88693ae26225353afb2a514b
N Bharath chowdary <bharath.n18189@gmail.com>
2025-11-10 07:15:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/',
+    build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+    },
+  },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
