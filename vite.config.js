af8c265846b0d49108bd5c3e9a1cb84f620e61f9
n-bharath-chowdary <bharath.n18189@gmail.com>
2025-11-10 07:43:12 +0530

    vite update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
+  base: '', 
+  build: {
     outDir: 'public/build',
     manifest: true,
     rollupOptions: {
@@ -12,10 +13,15 @@ export default defineConfig({
       ],
     },
   },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: {
+    https: true,
+    host: '0.0.0.0',
+    port: 5173,
+  },
 });
